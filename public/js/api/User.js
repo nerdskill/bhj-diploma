62819579e344841bb68e9dac7942d487de929580
nerdskill <alexeysiver@gmail.com>
2024-06-19 22:01:07 +0300

    initial commit api backend

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -1,39 +1,49 @@
-/**
- * Класс User управляет авторизацией, выходом и
- * регистрацией пользователя из приложения
- * Имеет свойство URL, равное '/user'.
- * */
 class User {
+
+  static URL = '/user';
+
   /**
    * Устанавливает текущего пользователя в
    * локальном хранилище.
-   * */
+   */
   static setCurrent(user) {
-
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
   /**
    * Удаляет информацию об авторизованном
    * пользователе из локального хранилища.
-   * */
+   */
   static unsetCurrent() {
-
+    localStorage.removeItem('user');
   }
 
   /**
    * Возвращает текущего авторизованного пользователя
    * из локального хранилища
-   * */
+   */
   static current() {
-
+    // Сокращаем реализацию метода с помощью использования метода getItem
+    return JSON.parse(localStorage.getItem('user'));
   }
 
   /**
    * Получает информацию о текущем
    * авторизованном пользователе.
-   * */
+   */
   static fetch(callback) {
-
+    createRequest({
+      url: this.URL + '/current',
+      method: 'GET',
+      callback: (err, response) => {
+        if (response && response.success) {
+          this.setCurrent(response.user);
+        } else {
+          this.unsetCurrent();
+        }
+        callback(err, response);
+      },
+    });
   }
 
   /**
@@ -41,7 +51,7 @@ class User {
    * После успешной авторизации необходимо
    * сохранить пользователя через метод
    * User.setCurrent.
-   * */
+   */
   static login(data, callback) {
     createRequest({
       url: this.URL + '/login',
@@ -62,16 +72,39 @@ class User {
    * После успешной авторизации необходимо
    * сохранить пользователя через метод
    * User.setCurrent.
-   * */
+   */
   static register(data, callback) {
-
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      responseType: 'json',
+      data,
+      callback: (err, response) => {
+        if (response && response.success && response.user) {
+          // Добавляем комментарий о проверке успешной регистрации
+          this.setCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
   }
 
   /**
    * Производит выход из приложения. После успешного
    * выхода необходимо вызвать метод User.unsetCurrent
-   * */
+   */
   static logout(callback) {
-
+    createRequest({
+      url: this.URL + '/logout',
+      method: 'POST',
+      responseType: 'json',
+      callback: (err, response) => {
+        if (response && response.success) {
+          // Добавляем комментарий о проверке успешного выхода
+          this.unsetCurrent();
+        }
+        callback(err, response);
+      },
+    });
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,41 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
-const createRequest = (options = {}) => {
+const createRequest = options => {
+  const xhr = new XMLHttpRequest();
+  const url = options.method === 'GET' ? `${options.url}?${new URLSearchParams(options.data).toString()}` : options.url;
 
+  // Устанавливаем метод запроса
+  xhr.open(options.method, url);
+
+  // Устанавливаем тип ответа
+  xhr.responseType = 'json';
+
+  // Создаем объект FormData для передачи данных в случае POST запроса
+  const formData = new FormData();
+
+  // Перебираем данные из параметра options.data и добавляем их в FormData
+  if (options.data) {
+    for (const key in options.data) {
+      formData.append(key, options.data[key]);
+    }
+  }
+
+ // Обработчик события завершения запроса
+  xhr.onload = () => {
+  // Запрос успешен
+    options.callback(null, xhr.response);
 };
+
+  // Обработчик события ошибки запроса
+  xhr.onerror = () => {
+    options.callback(new Error('Ошибка сети: не удалось выполнить запрос'), null);
+  };
+
+  try {
+    // Отправка запроса
+    xhr.send(options.method === 'GET' ? null : formData);
+    
+  } catch (e) {
+    // перехват сетевой ошибки
+    options.callback(e, null);
+  }
+};
+
